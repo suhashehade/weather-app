3f848bc78bde55129a3399c060397301817b0ccc
Suha523 <suhashehadeh123@gmail.com>
2023-02-11 16:46:14 +0200

    deloy setup

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "../dist")));
 app.use(express.static(path.join(__dirname, "../node_modules")));
 app.use("/.netlify/functions/server", api);
-// app.use('/', api)
+app.use('/', api)
 app.listen(PORT, function (err, res) {
   console.log(`The server listen on port ${PORT}`);
 });
