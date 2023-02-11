eb3d6158cf3f2a26ca8f675b24f68b91d6542f42
Suha523 <suhashehadeh123@gmail.com>
2023-02-11 18:18:13 +0200

    deloy setup

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ mongoose.connect(MONGO_URI)
 const PORT = 4040;
 const app = express();
 
-app.use(express.json());
+// app.use(express.json());
 app.use(bodyParser.json())
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "../dist")));
